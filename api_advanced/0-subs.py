a3a7171435eb4af76fdb3bc0cddb0f6282b2399e
JesseWK100 <j.walusansa@alustudent.com>
2025-02-15 17:10:53 +0200

    save

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -15,7 +15,7 @@ def number_of_subscribers(subreddit):
     headers = {"User-Agent": "custom_user_agent/0.0.1"}
     try:
         response = requests.get(url, headers=headers,
-                             allow_redirects=False, timeout=10)
+                                allow_redirects=False, timeout=10)
         if response.status_code != 200:
             return 0
         data = response.json()
